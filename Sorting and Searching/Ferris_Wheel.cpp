b982c5417b2962c3bfa41a19c194f0c6cc2344e4
cct1225 <grace951225@gmail.com>
2024-01-20 18:34:45 +0800

    20240120

---
+++ Sorting and Searching/Ferris_Wheel.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    long long int x;
+    vector<int>vec;
+    for(int i=0;i<n;i++)
+    {
+        int num;
+        cin>>num;
+        vec.push_back(num);
+    }
+}
